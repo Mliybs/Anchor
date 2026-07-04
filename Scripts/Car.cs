2fdae3cb41f3b6ef002451c78746be22cf0e9e2f
Jirigalang <149288921+Jirigalang@users.noreply.github.com>
2026-07-05 02:11:33 +0800

    外币巴伯

--- Scripts/Car.cs
+++ Scripts/Car.cs
@@ -1,3 +1,4 @@
+using Anchor.Scripts;
 using Godot;
 using System;
 
@@ -6,11 +7,16 @@ public partial class Car : CharacterBody2D
 	public const float Speed = 30.0f;
 	private float baseY;        // 原始高度
 	private bool jumpUp = false;
+
+	public AnchorBase Anchor { get; set; }
+
+	public Sprite2D Sprite;
 	public override void _Ready()
 	{
 		baseY = Position.Y;
+		Sprite = GetNode<Sprite2D>("Sprite2D");
 	}
-	public void Process(double delta)
+	public void 第一幕(double delta)
 	{
 		// 永远只前进，不改Y速度
 		Position += new Vector2(Speed * (float)delta, 0);
@@ -32,5 +38,27 @@ public partial class Car : CharacterBody2D
 			// 下一帧回到原位
 			Position = new Vector2(Position.X, baseY);
 		}
+	}
+
+	public void 开始第二幕()
+	{
+	}
+	public void 第二幕(double delta)
+	{
+		Sprite.Rotation += 50f * (float)delta;
+	}
+}
+
+
+public static class AnchorExtensions
+{
+	public static void ThrowToPosition(this AnchorBase anchor, CharacterBody2D chara, Vector2 position)
+	{
+		anchor.Freeze = false;
+		anchor.GetParent()?.RemoveChild(anchor);
+		chara.GetParent().AddChild(anchor);
+		anchor.Position = position;
+		var directionVector = new Vector2(-1, 1);
+		anchor.Rotation = (float)(directionVector.Angle() - (Math.PI / 180) * 90);
 	}
 }
--- Scripts/Openning.cs
+++ Scripts/Openning.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System;
 using System.Linq;
 
 public partial class Openning : Node2D
@@ -8,30 +9,108 @@ public partial class Openning : Node2D
 
 	[Export]
 	public Car Car;
+
+	[Export]
+	public MainCharacter Ako;
+
+	DateTime? FirstTime = null;
+
+	Func<double, int, int>[] 台本;
+
+	Camera2D camera;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Backgrounds = [.. GetChildren().OfType<Parallax2D>()];
 		Car = GetChildren().OfType<Car>().FirstOrDefault();
+		FirstTime ??= DateTime.Now;
+		台本 = [第一幕, 开始第二幕, 第二幕, 第三幕, 第四幕];
+		camera = GetChildren().OfType<Camera2D>().FirstOrDefault();
 	}
 
+	int 幕 = 0;
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		if (Input.IsActionJustPressed("ui_accept"))
 		{
-			GD.Print(delta.ToString());
+			GD.Print(DateTime.Now - FirstTime);
 		}
-		第一幕(delta);
+		if (幕 < 台本.Length)
+		{
+			幕 = 台本[幕](delta, 幕);
+		}
 	}
 
-	public void 第一幕(double delta)
+	public int 第一幕(double delta, int 幕)
 	{
+		GD.Print($"Car 节点名称: {Car.Name}");
+		GD.Print($"Car 是否在场景树中: {Car.IsInsideTree()}");
+		GD.Print($"Car 本地位置: {Car.Position}");
+		GD.Print($"Car 全局位置: {Car.GlobalPosition}");
+		GD.Print($"Car Sprite本地位置: {Car.Sprite.Position}");
+		GD.Print($"Car Sprite全局位置: {Car.Sprite.GlobalPosition}");
+		GD.Print($"Car 父节点: {Car.GetParent()?.Name}");
+		//汽车向前开，背景滚动
 		foreach (var background in Backgrounds)
 		{
 			background.ScrollOffset -= (float)delta * 100f * background.ScrollScale;
 		}
-		Car.Process(delta);
+		Car.第一幕(delta);
+		if (DateTime.Now - FirstTime > TimeSpan.FromSeconds(2))
+		{
+			return 幕 + 1;
+		}
+		return 幕;
 	}
 
+	public int 开始第二幕(double delta, int 幕)
+	{
+		Car.开始第二幕();
+		return 幕 + 1;
+	}
+	public int 第二幕(double delta, int 幕)
+	{
+		//某种神必力量让车停在了原地，背景停止滚动
+		Car.第二幕(delta);
+		if (Car.Sprite.Rotation > 3.14)
+		{
+			FirstTime = DateTime.Now;
+			return 幕 + 1;
+		}
+		return 幕;
+	}
+	private float shakeTime = 2f;
+	private float shakePower = 5f;
+	public int 第三幕(double delta, int 幕)
+	{
+		if (shakeTime > 0)
+		{
+			shakeTime -= (float)delta;
+
+			camera.Offset = new Vector2(
+				(float)GD.RandRange(-shakePower, shakePower),
+				(float)GD.RandRange(-shakePower, shakePower)
+			);
+			shakePower *= 0.9f; // 减小震动幅度
+		}
+		else
+		{
+			camera.Offset = Vector2.Zero;
+		}
+		if (shakePower < 0.1f)
+		{
+			return 幕 + 1;
+		}
+		return 幕;
+	}
+	public int 第四幕(double delta, int 幕)
+	{
+		var characterScene = ResourceLoader.Load<PackedScene>("res://Scenes/MainChara.tscn");
+		Ako = characterScene.Instantiate<MainCharacter>();
+
+		Ako.Position = Car.Position - new Vector2(0, 5);
+		AddChild(Ako);
+		return 幕 + 1;
+	}
 }
